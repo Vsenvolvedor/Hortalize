6f7179399c10f25473a202f0ae2fa7be43dac9a4
Vsenvolvedor <supermacalistein33@gmail.com>
2026-08-10 23:47:17 -0300

    Algoritmo para estruturacao do post ok

--- src/js/script.js
+++ src/js/script.js
@@ -14,12 +14,12 @@ async function doLastNewsElemention() {
     const lastnews_parent =document.querySelector(".news-list")
 
     dataJson.forEach((item, index) => {
-        if(index >= 3) return;
+        if(index < (dataJson.length-3)) return;
         lastnews_parent.innerHTML+= `
         <li>
-            <a href="">
+            <a href="./post.html?id=${item.id}">
                 <h3>
-                    ${item.title}
+                    ${item.title_short}
                 </h3>
                 <p>
                     ${item.short_desc}
@@ -35,13 +35,12 @@ async function doBlogPostList() {
     const post_parent = document.querySelector(".posts-list")
     
     dataJson.forEach((item) => {
-
         post_parent.innerHTML+= `
         <li>
             <a href="./post.html?id=${item.id}">
                 <img src="${item.image_short}" alt="">
                 <h2 class="section-title">
-                    ${item.title}
+                    ${item.title_short}
                 </h2>
                 <p>
                     ${item.short_desc}
@@ -52,3 +51,23 @@ async function doBlogPostList() {
     })
 }
 
+async function  doBlogPost() {
+    const dataJson = await loadData();
+    const idPost = new URLSearchParams(document.location.search).get("id")
+    const post = document.querySelector(".post-content")
+    const post_title = document.querySelector("#home h1 p")
+ 
+    dataJson.forEach((item) => {
+        if(item.id != idPost) return;
+        post_title.innerHTML = item.title
+        post.innerHTML+= `
+            <div class="post-image">
+                <img src="${item.image_big}" alt="">
+                <span>post por ${item.author}</span>
+            </div>
+            <p>
+                ${item.description}
+            </p>
+        `
+    })
+}
